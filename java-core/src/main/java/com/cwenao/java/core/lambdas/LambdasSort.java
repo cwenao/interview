6fd9ebe24b2a7a8ea0a616bccd0c6514c62738c3
cwenao <cwenao@126.com>
2017-11-22 16:11:51 +0800

    add the lambdas

---
+++ java-core/src/main/java/com/cwenao/java/core/lambdas/LambdasSort.java
@@ -0,0 +1,47 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.java.core.lambdas;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * @author cwenao
+ * @version $Id LambdasSort.java, v 0.1 2017-11-22 15:53 cwenao Exp $$
+ */
+public class LambdasSort {
+
+    public static void arraySortWithString(String[] arrays) {
+        Arrays.sort(arrays, (m, n) -> Integer.compare(m.length(), n.length()));
+        System.out.println(Arrays.toString(arrays));
+    }
+
+    public static void arraySortWithMultiple(String[] arrays) {
+        Arrays.sort(arrays, (m,n) ->{
+            if (m.length() > n.length()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        System.out.println(Arrays.toString(arrays));
+    }
+
+    public static void iteratorArrays(String[] arrays) {
+        Stream<String> stringStream = Stream.of(arrays);
+        stringStream.forEach(x -> System.out.println(x));
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"program", "creek", "is", "a", "java", "site"};
+//        arraySortWithString(arr);
+//        arraySortWithMultiple(arr);
+        iteratorArrays(arr);
+    }
+
+
+}
+
+
