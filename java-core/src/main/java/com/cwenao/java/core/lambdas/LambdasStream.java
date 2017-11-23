3a821aebb65998a63b124e3fd84a305706f990ab
cwenao <cwenao@126.com>
2017-11-23 10:02:50 +0800

    add the create stream

--- java-core/src/main/java/com/cwenao/java/core/lambdas/LambdasStream.java
+++ java-core/src/main/java/com/cwenao/java/core/lambdas/LambdasStream.java
@@ -4,9 +4,12 @@
  */
 package com.cwenao.java.core.lambdas;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 /**
  * @author cwenao
@@ -15,13 +18,31 @@ import java.util.List;
 public class LambdasStream {
 
     public static void listStream(List<String> list) {
-        list.stream().filter(s -> s == "RED").count();
-        System.out.println(list.stream().filter(s -> s == "RED").count());
+        //list.stream().filter(s -> s.equals( "RED")).count();
+        System.out.println(list.stream().filter(s -> s.equals("RED")).count());
     }
 
     public static void listStreamToArrays(List<String> list) {
         String[] arr = list.stream().filter(x -> !x.endsWith("RED")).toArray(String[]::new);
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void createStream() {
+        Stream<String> stringStream = Stream.generate(() -> "test").limit(10);
+        String[] arry = stringStream.toArray(String[]::new);
+        System.out.println(Arrays.toString(arry));
+    }
+
+    public static void createStreamUsedIterator() {
+        Stream<BigInteger> bigIntegerStream = Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.ONE)).limit(10);
+        BigInteger[] bigIntegers = bigIntegerStream.toArray(BigInteger[]::new);
+        System.out.println(Arrays.toString(bigIntegers));
+    }
+
+    public static void createStreamUsedPopularAPIs(String source, String pattern) {
+        Stream<String> words = Pattern.compile(pattern).splitAsStream(source);
+        String[] arrays = words.toArray(String[]::new);
+        System.out.println(Arrays.toString(arrays));
     }
 
     public static void main(String[] args) {
@@ -34,10 +55,16 @@ public class LambdasStream {
         list.add("cwenao");
         list.add("RED");
 
-        listStream(list);
+//        listStream(list);
 
-        listStreamToArrays(list);
+//        listStreamToArrays(list);
 
+        createStream();
+        createStreamUsedIterator();
+
+        String source = "hello world!123";
+        String pattern = "\\W";
+        createStreamUsedPopularAPIs(source,pattern);
     }
 
 }
