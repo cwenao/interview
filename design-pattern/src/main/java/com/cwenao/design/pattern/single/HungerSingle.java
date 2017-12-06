400731236acdee1ff13effb3ddb7c82f3344882a
cwenao <cwenao@126.com>
2017-12-06 10:53:24 +0800

    add the single

---
+++ design-pattern/src/main/java/com/cwenao/design/pattern/single/HungerSingle.java
@@ -0,0 +1,20 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.design.pattern.single;
+
+/**
+ * @author cwenao
+ * @version $Id HungerSingle.java, v 0.1 2017-12-06 10:42 cwenao Exp $$
+ */
+public class HungerSingle {
+    private static final HungerSingle hungerSingle = new HungerSingle();
+    private HungerSingle() {
+    }
+
+    public static HungerSingle getInstance() {
+        return hungerSingle;
+    }
+
+}
---
+++ design-pattern/src/main/java/com/cwenao/design/pattern/single/LazySingle.java
@@ -0,0 +1,25 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.design.pattern.single;
+
+/**
+ * @author cwenao
+ * @version $Id LazySingle.java, v 0.1 2017-12-06 10:50 cwenao Exp $$
+ */
+public class LazySingle {
+    private static LazySingle lazySingle;
+
+    private LazySingle() {
+
+    }
+
+    synchronized public static LazySingle getInstance() {
+        if (lazySingle == null) {
+            lazySingle = new LazySingle();
+        }
+        return lazySingle;
+    }
+}
+
