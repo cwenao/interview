ca6bc7855c68d2db6dd0e9551ca587b8a45076c2
cwenao <cwenao@126.com>
2017-12-11 07:01:49 +0800

    add the facade pattern

---
+++ design-pattern/src/main/java/com/cwenao/design/pattern/structuredpattern/facadepattern/FacadeSimple.java
@@ -0,0 +1,15 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.design.pattern.structuredpattern.facadepattern;
+
+/**
+ * @author cwenao
+ * @version $Id FacadeSimple.java, v 0.1 2017-12-11 06:13 cwenao Exp $$
+ */
+public interface FacadeSimple {
+    void fileReader();
+
+    void fileWrite();
+}
---
+++ design-pattern/src/main/java/com/cwenao/design/pattern/structuredpattern/facadepattern/FacadeSimpleImpl.java
@@ -0,0 +1,21 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.design.pattern.structuredpattern.facadepattern;
+
+/**
+ * @author cwenao
+ * @version $Id FacadeSimpleImpl.java, v 0.1 2017-12-11 06:20 cwenao Exp $$
+ */
+public class FacadeSimpleImpl implements FacadeSimple{
+    @Override
+    public void fileReader() {
+
+    }
+
+    @Override
+    public void fileWrite() {
+
+    }
+}
