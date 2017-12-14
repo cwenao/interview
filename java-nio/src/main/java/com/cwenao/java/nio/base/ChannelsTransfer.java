9b3b4d1b94154e8ea2b2ba6876bf06a27983e188
cwenao <cwenao@126.com>
2017-12-14 17:43:15 +0800

    add the scatter and two channel transfer

---
+++ java-nio/src/main/java/com/cwenao/java/nio/base/ChannelsTransfer.java
@@ -0,0 +1,60 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.java.nio.base;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author cwenao
+ * @version $Id ChannelsTransfer.java, v 0.1 2017-12-14 17:27 cwenao Exp $$
+ */
+public class ChannelsTransfer {
+
+    /**
+     * 从当前Channel直接传输到另外一个Channel
+     * @param fromPath
+     * @param toPath
+     */
+    public static void transferToAnother(String fromPath, String toPath) {
+        try {
+            RandomAccessFile accessFile = new RandomAccessFile(fromPath, "rw");
+            FileChannel fileChannelFrom = accessFile.getChannel();
+            RandomAccessFile toAccessFile = new RandomAccessFile(toPath, "rw");
+            FileChannel fileChannelTo = toAccessFile.getChannel();
+
+            long position = 0;
+            long limit = fileChannelFrom.size();
+            // transferTo、transferFrom
+            fileChannelFrom.transferTo(0, limit, fileChannelTo);
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+            while (fileChannelTo.read(byteBuffer) != -1) {
+                byteBuffer.flip();
+                fileChannelTo.write(byteBuffer);
+                byteBuffer.clear();
+            }
+            accessFile.close();
+            toAccessFile.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String fromPath = "/Users/cwenao/hexo_github_id_rsa";
+        String toPath = "/Users/cwenao/hexo_github_id_rsa_to";
+        transferToAnother(fromPath,toPath);
+    }
+
+}
---
+++ java-nio/src/main/java/com/cwenao/java/nio/base/ScatterAndGather.java
@@ -0,0 +1,61 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.java.nio.base;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author cwenao
+ * @version $Id ScatterAndGather.java, v 0.1 2017-12-14 17:01 cwenao Exp $$
+ */
+public class ScatterAndGather {
+
+    /**
+     * 使用sactter 从一个Channel往多个 ByteBuffer里面写数据
+     * Gather 从多个ByteBuffer里取数据往一个Channel写
+     * @param path
+     */
+    public static void scatterAndGather(String path) {
+
+        try {
+            RandomAccessFile accessFile = new RandomAccessFile(path, "rw");
+            ByteBuffer header = ByteBuffer.allocate(64);
+            ByteBuffer body = ByteBuffer.allocate(256);
+
+            ByteBuffer[] message = {header, body};
+
+            FileChannel fileChannel = accessFile.getChannel();
+
+            while (fileChannel.read(message) !=-1) {
+                header.flip();
+                while (header.hasRemaining()) {
+                    System.out.println(" this is the header message: " + (char) header.get());
+                }
+                body.flip();
+                while (body.hasRemaining()) {
+                    System.out.println(" this is the body message: " + (char) body.get());
+                }
+                header.clear();
+                body.clear();
+            }
+            accessFile.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void main(String[] args) {
+        String path = "/Users/cwenao/hexo_github_id_rsa";
+        scatterAndGather(path);
+    }
+
+}
