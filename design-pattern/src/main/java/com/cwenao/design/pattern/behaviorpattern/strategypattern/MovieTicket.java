0304424bea20b4cd73bd50bae18a09043a1316b7
cwenao <cwenao@126.com>
2017-12-18 23:10:14 +0800

    add the strategy pattern

---
+++ design-pattern/src/main/java/com/cwenao/design/pattern/behaviorpattern/strategypattern/MovieTicket.java
@@ -0,0 +1,12 @@
+/**
+ * Company
+ * Copyright (C) 2014-2017 All Rights Reserved.
+ */
+package com.cwenao.design.pattern.behaviorpattern.strategypattern;
+
+/**
+ * @author cwenao
+ * @version $Id MovieTicket.java, v 0.1 2017-12-18 23:09 cwenao Exp $$
+ */
+public class MovieTicket {
+}
